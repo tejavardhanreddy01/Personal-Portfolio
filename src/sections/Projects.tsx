e64a63967618a93baffdd4818bd3aeea43ca448b
tejavardhanreddy01 <avuthutejavardhanreddy@gmail.com>
2025-01-12 11:03:15 -0500

    Projects section completed and started with Tape section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,76 +1,99 @@
 import Image from "next/image";
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
+import CheckCircleIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg"
+import patriotWebLandingPage from "@/assets/images/patriot-web-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import garinImage from '@/assets/images/grain.jpg';
 
 const portfolioProjects = [
   {
-    company: "Acme Corp",
+    company: "GMU",
+    year: "2024",
+    title: "Patriot Web Redesign",
+    results: [
+      { title: "Improved usability and accessibility with React and Material-UI" },
+      { title: "Achieved compliance with WCAG standards" },
+      { title: "Enhanced user satisfaction by 35% through usability testing" },
+    ],
+    link: "https://patriot-web-redesign-production.up.railway.app/", 
+    image: patriotWebLandingPage,
+  },
+  {
+    company: "Personal Project",
+    year: "2024",
+    title: "Campus Survey Application",
+    results: [
+      { title: "Built a full-stack survey platform using Angular and Spring Boot" },
+      { title: "Optimized performance with AWS EC2 and S3 integration" },
+      { title: "Focused on form validation and dynamic routing" },
+    ],
+    link: "https://github.com/orgs/Campus-Survey-Project/repositories", // Replace with actual project link if available
+    image: lightSaasLandingPage, // Replace with a relevant image variable or path
+  },
+  {
+    company: "IQVIA",
     year: "2022",
-    title: "Dark Saas Landing Page",
+    title: "OCE Platform Enhancements",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: "Reduced system errors by 30% through feature migration" },
+      { title: "Boosted system performance by 20%" },
+      { title: "Automated manual processes, saving 12 hours per week" },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
-    image: darkSaasLandingPage,
-  },
-  {
-    company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
-    results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
-    ],
-    link: "https://youtu.be/7hi5zwO75yc",
-    image: lightSaasLandingPage,
-  },
-  {
-    company: "Quantum Dynamics",
-    year: "2023",
-    title: "AI Startup Landing Page",
-    results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
-    ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
-    image: aiStartupLandingPage,
+    //link: "https://www.linkedin.com/in/teja/", // Replace with actual project link if available
+    image: aiStartupLandingPage, // Replace with a relevant image variable or path
   },
 ];
 
+
 export const ProjectsSection = () => {
   return (
-  <div>
-    <div className="container">
-      <p>Real-world Results</p>
-      <h2>Featured Projects</h2>
-      <p>See how i transformed concepts into engaging digital experience</p>
-      <div>
-        {portfolioProjects.map((project) => (
-          <div key={project.title}>
-            <div>
-              <span>{project.company}</span>
-              <span>{project.year}</span>
+    <section className="pb-16 lg:py-24">
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400  text-center bg-clip-text text-transparent">Real-world Results</p>
+        </div>
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">Featured Projects</h2>
+        <p className="text-center md:text-lg lg:text-xl text-white/16 mt-4 max-w-md mx-auto">See how i transformed concepts into engaging digital experience</p>
+        <div className="mt-10 md:mt-20 flex flex-col gap-20">
+          {portfolioProjects.map((project) => (
+            <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none">
+              <div className="absolute inset-0 -z-10 opacity-5" style={{
+                backgroundImage: `url(${garinImage.src})`,
+              }}></div>
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                  <span>{project.company}</span>
+                  <span>&bull;</span>
+                  <span>{project.year}</span>
+                </div>
+              
+              <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>
+              <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+              <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                {project.results.map((result) => (
+                  <li className="flex gap-2 text-sm md:text-base text-white/50">
+                    <CheckCircleIcon className="size-5 md:size-6" />
+                    <span>{result.title}</span>
+                  </li>
+                ))}
+              </ul>
+              <a href={project.link}>
+                <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                  <span>Visit Live Site</span>
+                  <ArrowUpRightIcon className="size-4"/>
+                  </button>
+              </a>
+              </div>
+              <div className="relative">
+              <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"/>
+              </div>
+              </div>
             </div>
-            <h3>{project.title}</h3>
-            <hr />
-            <ul>
-              {project.results.map((result) => (
-                <li>{result.title}</li>
-              ))}
-            </ul>
-            <a href={project.link}>
-              <button>View Live Site</button>
-            </a>
-            <Image src={project.image} alt={project.title} />
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  </div>
+    </section>
   );
 };
