26761e6d0c417e1d0c492e4ccb4c06e8b68c8728
tejavardhanreddy01 <avuthutejavardhanreddy@gmail.com>
2025-01-14 23:34:49 -0500

    Refactor: used string literals for the production build

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -18,7 +18,7 @@ export const ContactSection = () => {
                 {'Let\'s create something amazing together'}
               </h2>
               <p className="text-sm md:text-base mt-2">
-              Excited to bring your next project to life. Let's connect and collaborate!
+              {'Excited to bring your next project to life. Let\'s connect and collaborate!'}
               </p>
               <p className="text-sm md:text-base mt-2">
               Yep, still working on this portfolio. Progress is happening... slowly but surely!
